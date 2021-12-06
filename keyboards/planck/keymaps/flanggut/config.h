986b53f63e87499c05f28fcf91e02aa0a9681870
Fabian Langguth <flanggut@fb.com>
2021-12-06 09:31:25 +0100

    planck: updates

--- keyboards/planck/keymaps/flanggut/config.h
+++ keyboards/planck/keymaps/flanggut/config.h
@@ -16,6 +16,8 @@
 
 #pragma once
 
+#define TAPPING_FORCE_HOLD
+
 #ifdef AUDIO_ENABLE
 #    define STARTUP_SONG SONG(PLANCK_SOUND)
 // #define STARTUP_SONG SONG(NO_SOUND)
--- keyboards/planck/keymaps/flanggut/keymap.c
+++ keyboards/planck/keymaps/flanggut/keymap.c
@@ -51,6 +51,7 @@ enum planck_keycodes {
 #define FL_ESC LALT_T(KC_ESCAPE)
 #define FL_TL LGUI(LSFT(KC_LBRACKET))
 #define FL_TR LGUI(LSFT(KC_RBRACKET))
+#define FL_Z LCTL_T(KC_Z)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -68,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,         KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     FL_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,         KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,         KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+    KC_LSFT, FL_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,         KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
     BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_LCTL, LT(4,KC_SPC), RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
@@ -92,19 +93,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  | Left | Down |  Up  |Right |   ]  |      |
+ * | Del  |   `  |  [   |  (   |   )  |   ]  | Left | Down |  Up  |Right |   ]  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |  \   |
+ * |      |   ~  |  |   |  {   |   }  |   \  |  F12 |ISO # |ISO / |Pg Up |Pg Dn |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_RBRC, _______,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_BSLS,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+    KC_DEL,  KC_GRV,  KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_RBRC, _______,
+    _______, KC_TILD, KC_PIPE, KC_LCBR, KC_RCBR, KC_BSPC, KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_BSLS,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
@@ -131,17 +132,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * | Del  |   `  |  [   |  (   |   )  |   ]  |  *   |   -  |  +   |  =   |  ]   |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   _  |      |      |      |      |
+ * |      |   ~  |  |   |  {   |   }  |   \  |  F12 |   _  |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LAYER4] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_UNDS, _______, _______, _______, _______,
+    KC_DEL,  KC_GRV,  KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_ASTR, KC_MINS, KC_PLUS, KC_EQL,  KC_RBRC, KC_BSLS,
+    _______, KC_TILD, KC_PIPE, KC_LCBR, KC_RCBR, KC_BSPC, KC_F12,  KC_UNDS, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
